45af7a1925b51873385056848c0677651f33fbc6
Marco A. Rojas <marco.rojas@alumno.buap.mx>
2019-09-24 14:55:00 -0400

    Function Components

--- src/App.js
+++ src/App.js
@@ -28,36 +28,29 @@ class ProductTableProvider extends React.Component {
     }
 }
 
-class ProductCategoryRow extends React.Component {
-    render() {
-        const category = this.props.category;
-        return (
-            <tr>
-                <th colSpan="2">
-                    {category}
-                </th>
-            </tr>
-        );
-    }
-}
+const ProductCategoryRow = props => (
+    <tr>
+        <th colSpan="2">
+            {props.category}
+        </th>
+    </tr>
+);
 
-class ProductRow extends React.Component {
-    render() {
-        const product = this.props.product;
-        const name = product.stocked ?
-            product.name :
-            <span style={{color: 'red'}}>
+const ProductRow = props => {
+    const product = props.product;
+    const name = product.stocked ?
+        product.name :
+        <span style={{color: 'red'}}>
                 {product.name}
             </span>;
 
-        return (
-            <tr>
-                <td>{name}</td>
-                <td>{product.price}</td>
-            </tr>
-        );
-    }
-}
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{product.price}</td>
+        </tr>
+    );
+};
 
 const ProductTable = () => {
     const context = useContext(AppContext);
